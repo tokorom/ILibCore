6bb9a3357c40ddca8dd3afdba2c874a31c5c433e
tokorom <tokorom@gmail.com>
2011-12-11 22:34:16 +0900

    update NSLog macros for debugging

--- ILibDebug.h
+++ ILibDebug.h
@@ -2,66 +2,21 @@
 //  ILibDebug.h
 //
 //  Created by ToKoRo on 2009-12-03.
-//  Last Change: 2011-07-10.
+//  Last Change: 2011-12-11.
 //
 
-#import <objc/runtime.h>
+#define NSLogFunction() NSLog( @"%s", __func__ )
+#define NSLogLine() NSLog( @"%s: %d", __FILE__, __LINE__ )
+#define NSLogStackTarce() NSLog( @"Stack trace: %@", [NSThread callStackSymbols] )
 
-#if defined(DEBUG)
-
-//FOUNDATION_EXPORT void NSLog(NSString *format, ...) __attribute__((format(__NSString__, 1, 2)));
-//FOUNDATION_EXPORT void NSLogv(NSString *format, va_list args);
+#if defined(DEBUG) //< A
 
 #if !defined(NSLog)
 #define NSLog( args... ) NSLog( args )
 #endif
 
-#define ILibDebugLog( str ) \
-{ \
-  for ( int i = 0; i < [str length]; i += 64 ) { \
-    NSLog( [str substringWithRange:NSMakeRange( i, 64 )] ); \
-  } \
-}
+#else //< A
 
-#else
-
-#if !defined(NSLog)
 #define NSLog( args... )
-#endif
 
-#define ILibDebugLog( str )
-
-#endif
-#if defined(DEBUG)
-
-#if !defined(NSLog)
-#define NSLog( args... ) NSLog( args, 0 )
-#endif
-
-#else
-
-#if !defined(NSLog)
-#define NSLog( args... )
-#endif
-
-#endif
-
-/**
- * ObjectのProperty一覧をデバッグ出力する 
- */
-static void ILDebugPrintPropertyList( id object ) {
-#ifdef DEBUG
- unsigned int outCount, i;
- objc_property_t *properties = class_copyPropertyList([object class], &outCount);
- for(i = 0; i < outCount; i++) {
-  objc_property_t property = properties[i];
-  const char *propName = property_getName(property);
-  const char *propType = property_getAttributes(property);
-  NSString *propertyName = [NSString stringWithUTF8String:propName];
-  NSString *propertyType = [NSString stringWithUTF8String:propType];
-  NSLog(@"- %@: %@", propertyName, propertyType);
- }
- free(properties);
-#endif
-}
-
+#endif //< A
