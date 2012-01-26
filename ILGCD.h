3c3a2f6e5cdeee8b346a162003e2fecf8d8bda24
tokorom <tokorom@gmail.com>
2012-01-26 23:29:16 +0900

    add MAIN_THREAD macro and GLOBAL_THREAD macro

--- ILGCD.h
+++ ILGCD.h
@@ -2,10 +2,13 @@
 //  ILGCD.h
 //
 //  Created by ToKoRo on 2011-06-19.
-//  Last Change: 2011-08-04.
+//  Last Change: 2012-01-26.
 //
 
 #define MAIN_QUEUE( th ) dispatch_async( dispatch_get_main_queue(), ^{ th } )
 #define GLOBAL_QUEUE( th ) dispatch_async( dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0 ), ^{ th } )
+
+#define MAIN_THREAD( bl ) dispatch_async( dispatch_get_main_queue(), bl )
+#define GLOBAL_THREAD( bl ) dispatch_async( dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0 ), bl )
 
 typedef void (^ILGCDBlock_b)(void);
